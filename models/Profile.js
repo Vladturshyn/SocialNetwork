ac86972aac74ddbd909944bc077f1faabd9b058c
Vlad <vturshyn@gmail.com>
2018-10-02 14:43:21 +0300

    user profile route

--- models/Profile.js
+++ models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // create profile schema constructor
-const ProfileSchema = new Schema({
+const profileSchema = new Schema({
    user: {
     type: Schema.Types.ObjectId,
     ref: 'users'
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
-router.get('/test', (request,response)=>{
-    response.json({ms:'profile'})
-});
-
+// Load Profile models
+const Profile = require('../../models/Profile');
+// Load User Profile models
+const User = require('../../models/User');
+// Get user profile
+router.get('/', 
+    passport.authenticate('jwt', { session: false } ),
+    (request, response)=>{
+    const errors = {};
+    Profile.findOne({user: request.user.id})
+        .then(profile =>{
+            errors.noprofile = "There is no profile for this user"
+            if(!profile){
+                response.status(404).json(errors);
+            }else{
+                response.json(profile);
+            }
+        }).catch(err => console.log(err));
+})
 module.exports = router;
