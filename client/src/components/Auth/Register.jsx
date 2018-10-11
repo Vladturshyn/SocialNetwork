beab58d6b2b57c9378cd0a10761a88e09c34aaaa
Vlad <vturshyn@gmail.com>
2018-10-11 22:48:38 +0300

    test registration

--- client/src/components/Auth/Register.jsx
+++ client/src/components/Auth/Register.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export default class Reginster extends Component {
     state = {
@@ -22,8 +23,11 @@ export default class Reginster extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
-  }
+    axios
+      .post('/api/users/register', newUser)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err.response.data));
+  };
 
   render() {
     return (
