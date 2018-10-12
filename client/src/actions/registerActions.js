e45d5f9edd209d8b8373416b35402b340a4aaf9b
Vlad <vturshyn@gmail.com>
2018-10-12 13:49:09 +0300

    utils/setAuthToken func

--- client/src/actions/registerActions.js
+++ client/src/actions/registerActions.js
@@ -1,13 +1,32 @@
 import {GET_ERRORS} from './types';
+import setAuthToken from '../utils/setAuthToken';
 import axios from 'axios';
 
 // register user
-export const registerUser = (userDate,history) => dispatch => {
+export const registerUser = (userData,history) => dispatch => {
     axios
-      .post('/api/users/register', userDate)
+      .post('/api/users/register', userData)
       .then(res => history.push('/login'))
       .catch(err => dispatch({
           type: GET_ERRORS,
           payload: err.response.data
       }));
+}
+
+// login. get user token
+
+export const loginUser = (userData) => dispatch =>{
+    axios.post('/api/users/login', userData)
+        .then(res=>{
+            // Save to localStorage
+            const { token } = res.data;
+            // Set token to localStorage
+            localStorage.getItem('jwtToken',token);
+            // Set token to Auth header
+            setAuthToken(token);
+        })
+        .catch(err => dispatch({
+            type: GET_ERRORS,
+            payload: err.response.data})
+        );
 }
--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -17,7 +17,6 @@ export default class Login extends Component {
       email: this.state.email,
       password: this.state.password
     };
-    console.log(user);
   }
 
   render() {
